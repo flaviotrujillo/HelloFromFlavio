af13a4e6e92e47eafc1a32a0cef0492eaff6cbcc
Paul Humphreys <humphrep@yahoo.com>
2021-01-27 09:52:02 -0600

    Added Paul's Response

--- src/trujillo/Main.java
+++ src/trujillo/Main.java
@@ -6,5 +6,8 @@ public class Main {
 	// Flavio say hello
         String myname3 = "Flavio";
         System.out.println("Hello, my name is " + myname3 + ".");
+
+        String paulName = "Paul";
+        System.out.printf("Hello %s, my name is %s.%n", myname3, paulName );
     }
 }
