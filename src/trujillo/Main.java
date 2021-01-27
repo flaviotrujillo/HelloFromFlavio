dc70e4e5eacbdfb68a063a511574c4abb7c1e2db
meflav <tru19006@byui.edu>
2021-01-26 21:58:47 -0500

    Initial commit

---
+++ src/trujillo/Main.java
@@ -0,0 +1,10 @@
+package trujillo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Flavio say hello
+        String myname3 = "Flavio";
+        System.out.println("Hello, my name is " + myname3 + ".");
+    }
+}
